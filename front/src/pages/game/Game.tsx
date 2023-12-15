201d4025c78df7f7a1c43e465c87f6c178b72767
jvldrm <jes.valderrama@gmail.com>
2023-12-14 23:20:41 -0800

    Button end and start game

--- front/src/pages/game/Game.tsx
+++ front/src/pages/game/Game.tsx
@@ -1,9 +1,12 @@
 
 import "./Game.css";
-import React, {FC, ReactNode, SyntheticEvent} from "react";
+import React, { FC, ReactNode, SyntheticEvent } from "react";
 
 import rook_w from "./Chess_rlt45.svg";
-import {ChessGame} from "./ChessGame";
+import { ChessGame } from "./ChessGame";
+import { PlayButton } from "./PlayButton";
+import { EndButton } from "./EndButton";
+import { HStack, VStack} from "@chakra-ui/react";
 
 
 function Game() {
@@ -11,8 +14,16 @@ function Game() {
   return (
     <>
       <h1>The game page</h1>
-      <ChessGame />
-      
+      <VStack>
+
+
+        <ChessGame />
+        <HStack>
+          <PlayButton />
+          <EndButton />
+        </HStack>
+      </VStack>
+
     </>
 
   );
